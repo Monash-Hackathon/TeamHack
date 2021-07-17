144ff0e06521d6084cda119ad661bfb60c7da097
Chok Ming Jie <mcho0068@student.monash.edu>
2021-07-17 21:47:59 +0800

    ss

--- compare.js
+++ compare.js
@@ -5,7 +5,10 @@ let dictionary = {
     "R": {
         name: "Realistic",
         description: "These people are often good at mechanical or atheletic jobs.",
-        pathway: ["Agriculture","Health Assistant","Computers","Construction","Mechanic/Machinist","Engineering","Food and Hospitality"]
+        pathway: ["Agriculture","Health Assistant","Computers","Construction","Mechanic/Machinist","Engineering","Food and Hospitality"],
+        gif: ["https://i.pinimg.com/originals/d5/69/12/d56912e297cf3a797d5d5c728a23fda9.gif", "https://cdn.dribbble.com/users/1129235/screenshots/10762000/media/ace6e95b814e602a1b5a0527a54af10f.gif",
+        "https://cdn.dribbble.com/users/2008861/screenshots/15526868/alfa-office-dude.gif", "https://www.esthete.in/images/icon2.gif", "https://cdn.dribbble.com/users/2221597/screenshots/11933222/media/c7bff7d4fa3f4befecb1cd1ae87c08ae.gif", "https://blog.advids.co/wp-content/uploads//2017/06/industry-tech111-min.gif",
+        "http://discovertemplate.com/wp-content/uploads/2020/09/Hospitality-Services-Animated-GIF-Icon-pack.gif"]
     },
 
     "I": {
@@ -64,7 +67,7 @@ function displayResults(attribute){
     {
         output += ` <div class="col">
             <div class="card h-100">
-              <img src="img/giphy.gif" class="card-img-top" alt="...">
+              <img src="${dictionary[attribute].gif[i]}" class="card-img-top" alt="...">
               <div class="card-body">
                 <h5 class="card-title">${dictionary[attribute].pathway[i]}</h5>
                 <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
