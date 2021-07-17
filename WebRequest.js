b0b3b91325fa98011e56cc66244796223eeb7079
Ken Wilbert Osman <kosm0003@student.monash.edu>
2021-07-17 15:23:56 +0700

    again

--- WebRequest.js
+++ WebRequest.js
@@ -35,28 +35,28 @@ function jobSearch(jobName, location, callbackName) {
 
     //Define a variable to hold the data needed to be requested
     let data = {
-        engine = "google_jobs",
+        engine : "google_jobs",
         q: `${jobName}`,
-        location = `${location}`,
-        api_key= "6f147a45afa10165eb8f514be29ee2ebde68971252c901c0779f9936a2ed19e3",
-        callback: callbackName
+        location : `${location}`,
+        api_key: "6f147a45afa10165eb8f514be29ee2ebde68971252c901c0779f9936a2ed19e3",
+        json: callbackName
     }
 
     //Generate request to the web service
-    webServiceRequest(`https://serpapi.com/search.json?`, data);
+    webServiceRequest(`https://serpapi.com/search.json`, data);
 }
 
 function jobApply(jobId, callbackName) {
     //Define a variable to hold the data needed to be requested
     let data = {
-        engine = "google_jobs_listing",
+        engine : "google_jobs_listing",
         q: `${jobId}`,
-        api_key= "6f147a45afa10165eb8f514be29ee2ebde68971252c901c0779f9936a2ed19e3",
-        callback: callbackName
+        api_key: "6f147a45afa10165eb8f514be29ee2ebde68971252c901c0779f9936a2ed19e3",
+        output: callbackName
     }
 
     //Generate request to the web service
-    webServiceRequest(`https://serpapi.com/search.json?`, data);
+    webServiceRequest(`https://serpapi.com/search.json`, data);
 }
 
 function abc() {
@@ -68,7 +68,5 @@ function abc() {
 
 function showData(result) {
     let displayRef = document.getElementById("display");
-
-    let data = result.jobs_results[0];
-    displayRef.innerHTML = data.title;
+    displayRef.innerHTML = result
 }
